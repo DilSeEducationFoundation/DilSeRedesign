b6ebbb58a3773489b04e91277f096c40d552af80
naveenpallayil <naveennairp@gmail.com>
2016-12-27 15:25:56 +0530

    Fixed UX comments and updated contact functinoality

--- DilSeRedesign/js/index.js
+++ DilSeRedesign/js/index.js
@@ -1,10 +1,21 @@
 ﻿"use strict";
 $(document).ready(function () {
     renderImages();
+    configureRenderMoreText();
 });
 var sendMail = function () {
-    var link = 'mailto:test@test.com?subject=' +
-                   $("input[name='first_name']").val() + '&body=' + $("input[name='last_name']").val();
+    var contributionType = [];
+    $("input:checkbox[name=type]:checked").each(function(){
+        contributionType.push($(this).val());
+    });
+    var link = 'mailto:test@test.com?subject=Dilse Foundation: association request'
+                   + '&body=First Name: ' + $("input[name='first_name']").val()
+                   + '%0ALast Name: ' + $("input[name='last_name']").val()
+                   + '%0AEmail: ' + $("input[name='email']").val()
+                   + '%0APhone: ' + $("input[name='phone']").val()
+                   + '%0AAddress: ' + $("input[name='address']").val()
+                   + '%0AMessage: ' + $("textarea[name='comment']").val()
+                   + '%0AAssociation type:%0A' + contributionType.join("%0A");
     window.location.href = link;
 
 },
@@ -33,4 +44,38 @@ renderImages = function () {
             });
         });
     });
-};
+
+},
+configureRenderMoreText = function(){
+    var showChar = 300;
+    var ellipsestext = "...";
+    var moretext = "more";
+    var lesstext = "less";
+    $('.more').each(function() {
+        var content = $(this).html();
+
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar-1, content.length - showChar);
+
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+}
